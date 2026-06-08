d5c566bfafcd14e545f3ab57420b5bfb0471e9b2
EricQSC <anacmmanulc@gmail.com>
2026-06-08 16:14:43 +0100

    tentativa 3 barra notificações

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'pt.ipvc.sempreabulir',
-  appName: 'SempreABulir',
+  appId: 'pt.ipvc.sempreabulir.app', // (Mantém o ID que já tens)
+  appName: 'SempreABulir', // (Mantém o nome que já tens)
   webDir: 'www',
-  server: {
-    androidScheme: 'https'
+  // Adiciona este bloco de plugins:
+  plugins: {
+    StatusBar: {
+      backgroundColor: '#f4c20d', // O teu Amarelo Académico
+      style: 'LIGHT' // Coloca os ícones da bateria e horas a preto!
+    }
   }
 };
 
